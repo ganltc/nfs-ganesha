e10eaa65b20d372f112b095f948d06f9285b1ed8
Malahal Naineni <malahal@us.ibm.com>
2017-12-11 17:01:23 +0530

    IBM-only: Move open_fd_count from MDCACHE to FSALs.
    FSAL needs to close a file descriptor, if needed, and then open. It is
    hard to pass this information back to the upper layers. It is lot easier
    to modify this counter in the FSAL itself than in other layers. Of
    course, MDCACHE needs to use it to close files though.
    
    Change-Id: I1eea7c57d8dba88caf4f632958f714c0e7403b1c
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 8dc92882ef85b0424eb0556b0975e41f04489714)

--- src/FSAL/commonlib.c
+++ src/FSAL/commonlib.c
@@ -1537,8 +1537,6 @@ again:
 
 		if (not_open_usable(my_fd->openflags, openflags)) {
 			if (my_fd->openflags != FSAL_O_CLOSED) {
-				ssize_t count;
-
 				/* Add desired mode to existing mode. */
 				try_openflags = openflags | my_fd->openflags;
 
@@ -1553,12 +1551,6 @@ again:
 						 msg_fsal_err(status.major));
 					*has_lock = false;
 					return status;
-				}
-				count = atomic_dec_size_t(&open_fd_count);
-				if (count < 0) {
-					LogCrit(COMPONENT_FSAL,
-					    "open_fd_count is negative: %zd",
-					    count);
 				}
 			} else if (openflags == FSAL_O_ANY) {
 				try_openflags = FSAL_O_READ;
@@ -1591,8 +1583,6 @@ again:
 				*has_lock = false;
 				return status;
 			}
-
-			(void) atomic_inc_size_t(&open_fd_count);
 		}
 
 		/* Ok, now we should be in the correct mode.
