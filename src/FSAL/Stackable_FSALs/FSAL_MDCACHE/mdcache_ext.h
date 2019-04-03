f053ac7afcabd4ec200337db75034b4087961ed8
Malahal Naineni <malahal@us.ibm.com>
2019-04-03 16:34:37 +0530

    IBM-only: Set FD_LWMark_Percent to 0
    This setting will cause the lru run to close all possible open file
    descriptors. This is close to setting FD_Cache to FALSE in the older
    NFS-Ganesha versions. This way there is a fair chance that Samba could
    open files at least after the LRU run.
    
    Change-Id: I4a7c523f163cfc0b860f5d5f3ce8bad5654d41e0
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 825c49cfb1b83cbc1ec9c8797cb55500fd6f8d71)

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_ext.h
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_ext.h
@@ -104,7 +104,8 @@ struct mdcache_parameter {
 	uint32_t fd_hwmark_percent;
 	/** The percentage of the system-imposed maximum of file
 	    descriptors below which Ganesha will not reap file
-	    descriptors.  Defaults to 50, settable with FD_LWMark_Percent. */
+	    descriptors.  Defaults to 0,
+	    settable with FD_LWMark_Percent. */
 	uint32_t fd_lwmark_percent;
 	/** Roughly, the amount of work to do on each pass through the
 	    thread under normal conditions.  (Ideally, a multiple of
