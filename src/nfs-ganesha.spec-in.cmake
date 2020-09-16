bc47edd3d9409e6b46a91ec820f552c50ce560d5
Malahal Naineni <malahal@us.ibm.com>
2020-09-16 18:58:49 +0530

    IBM-only: [scale] Avoid building unneeded rpms
    We build unneeded packages with current build process. The build team
    deletes packages that Scale doesn't need. It would be better to build
    only the packages we need. Changed rpm spec file to remove some hard
    coded packages and others using the cmake options.
    
    Also, added a build script for rpms just like our debs build script. We
    can make changes to this build script to run cmake with exact options we
    need without asking the build team to modify their scipts.
    
    Change-Id: I98bb08877ad693f593b7bed125d1f0ddbc3061a0
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 466f3f00b75420ad043448f2bcee67fa23364758)

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -224,27 +224,6 @@ Requires: nfs-ganesha = %{version}-%{release}
 This package contains a FSAL shared object to
 be used with NFS-Ganesha to support VFS based filesystems
 
-%package proxy-v4
-Summary: The NFS-GANESHA PROXY_V4 FSAL
-Group: Applications/System
-BuildRequires: libattr-devel
-Requires: nfs-ganesha = %{version}-%{release}
-
-%description proxy-v4
-This package contains a FSAL shared object to
-be used with NFS-Ganesha to support PROXY_V4 based filesystems
-
-%package proxy-v3
-Summary: The NFS-GANESHA PROXY_V3 FSAL
-Group: Applications/System
-BuildRequires: libattr-devel
-Requires: nfs-ganesha = %{version}-%{release}
-
-%description proxy-v3
-This package contains a FSAL shared object to
-be used with NFS-Ganesha to support PROXY_V3 based filesystems
-
-
 %if %{with utils}
 %package utils
 Summary: The NFS-GANESHA util scripts
@@ -526,9 +505,9 @@ cmake .	-DCMAKE_BUILD_TYPE=Debug			\
 	-DUSE_RADOS_RECOV=%{use_rados_recov}		\
 	-DRADOS_URLS=%{use_rados_urls}			\
 	-DUSE_FSAL_VFS=ON				\
-	-DUSE_FSAL_PROXY_V4=ON				\
+	-DUSE_FSAL_PROXY=OFF				\
 	-DUSE_DBUS=ON					\
-	-DUSE_9P=%{use_9P}				\
+	-DUSE_9P=OFF					\
 	-DDISTNAME_HAS_GIT_DATA=OFF			\
 	-DUSE_MAN_PAGE=%{use_man_page}                  \
 	-DRPCBIND=%{use_rpcbind}			\
@@ -542,11 +521,6 @@ cmake .	-DCMAKE_BUILD_TYPE=Debug			\
 %endif
 
 make %{?_smp_mflags} || make %{?_smp_mflags} || make
-
-%if ( 0%{?fedora} >= 30 || 0%{?rhel} >= 8 )
-make -C selinux -f /usr/share/selinux/devel/Makefile ganesha.pp
-pushd selinux && bzip2 -9 ganesha.pp && popd
-%endif
 
 %install
 mkdir -p %{buildroot}%{_sysconfdir}/ganesha/
@@ -565,7 +539,6 @@ install -m 755 scripts/gen_ctdb_epoch.py %{buildroot}%{_libexecdir}/ganesha/
 %if %{with 9P}
 install -m 755 tools/mount.9P	%{buildroot}%{_sbindir}/mount.9P
 %endif
-
 install -m 644 config_samples/vfs.conf %{buildroot}%{_sysconfdir}/ganesha
 
 mkdir -p %{buildroot}%{_unitdir}
@@ -728,12 +701,6 @@ exit 0
 %config(noreplace) %{_sysconfdir}/ganesha/vfs.conf
 %if %{with man_page}
 %{_mandir}/*/ganesha-vfs-config.8.gz
-%endif
-
-%files proxy-v4
-%{_libdir}/ganesha/libfsalproxy_v4*
-%if %{with man_page}
-%{_mandir}/*/ganesha-proxy-v4-config.8.gz
 %endif
 
 %files proxy-v3
