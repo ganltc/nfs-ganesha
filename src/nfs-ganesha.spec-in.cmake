292167c8bbbf0943df47921d159c8354e119a959
Anuradha lella <Anuradha@li-0892024c-2c4b-11b2-a85c-fe75cc3c19d4.ibm.com>
2021-07-08 01:38:44 +0530

    IBM-only: Added the support of librcu in sles and Fixed the build error. LIBURCU support in needed in 3.5 code.fixed the build error by adding the code to fix this.

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -117,7 +117,6 @@ BuildRequires:	cmake
 BuildRequires:	bison
 BuildRequires:	flex
 BuildRequires:	pkgconfig
-BuildRequires:	userspace-rcu-devel
 BuildRequires:	krb5-devel
 %if %{with rados_recov} || %{with rados_urls}
 BuildRequires: librados-devel >= 0.61
@@ -135,6 +134,12 @@ Requires:	dbus-1
 %else
 BuildRequires:	dbus-devel
 Requires:	dbus
+%endif
+
+%if ( 0%{?suse_version} )
+BuildRequires:  liburcu-devel
+%else
+BuildRequires: userspace-rcu-devel
 %endif
 
 %if ( 0%{?suse_version} )
