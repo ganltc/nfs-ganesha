4961c8cdbd1dae42d5d9e15bf30717688176e1c7
Alok Chandra Mallick <alomalli@in.ibm.com>
2022-04-23 19:23:28 +0530

    IBM-only: Separate debian packages based on Ubuntu Codename

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,4 +1,44 @@
-#!/bin/sh
+#!/bin/bash
+
+DIRNAME=$(dirname $0)
+VERFILE=${DIRNAME}/GANESHA_VERSION
+
+# Read GANESHA_VERSION file
+. ${VERFILE}
+if (( $? != 0 ))
+then
+    echo "Unable to parse GANESHA_VERSION file. Aborting!!!"
+    exit 1
+fi
+
+# Detect Ubuntu Code name
+if [[ ! -r /etc/os-release ]]
+then
+    echo "Unable to detect build os. Aborting!!!"
+    exit 1
+fi
+. /etc/os-release
+
+case "${UBUNTU_CODENAME}" in
+    focal)
+    OS_RELEASE="~${UBUNTU_CODENAME}"
+    ;;
+
+    jammy)
+    OS_RELEASE="~${UBUNTU_CODENAME}"
+    ;;
+
+    *)
+    echo "Unsupported build os (${UBUNTU_CODENAME}). Aborting!!!"
+    exit 1
+esac
+
+GANESHA_VERSION_STRING="$GANESHA_MAJOR_VERSION.$GANESHA_MINOR_VERSION-$GANESHA_TAG$OS_RELEASE"
+echo "Ganesha packages with version : $GANESHA_VERSION_STRING will be generated."
+
+#Update the changelog file with new Ganesha version and Ubuntu codename
+debchange --newversion $GANESHA_VERSION_STRING --distribution $UBUNTU_CODENAME "NFS Ganesha release $GANESHA_VERSION_STRING for Spectrum Scale" --force-bad-version
+
 rm -r src/debian
 echo "Creating build dependencies ..."
 mk-build-deps --install debian/control
