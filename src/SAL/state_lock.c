a6d22c2e316414a1b7351019d0dc607a420d03c9
Steve Lord <steve.p.lord@gmail.com>
2024-10-16 09:34:06 -0500

    Avoid deref of uninitialized pointer
    Fix for Issue #1186
    
    In the NFS V3 case nfs4_clientid was not
    initialized and the logic in do_lock_op
    was testing for it being set causing a
    panic.
    
    Change-Id: Icfd95cfbf750affd600ac04ec0faeb3ebfdda2c7
    Signed-off-by: Steve Lord <steve.p.lord@gmail.com>

--- src/SAL/state_lock.c
+++ src/SAL/state_lock.c
@@ -2164,7 +2164,7 @@ state_status_t do_lock_op(struct fsal_obj_handle *obj,
 #ifdef _USE_NLM
 	struct state_nlm_client_t *nlm_client;
 #endif
-	struct nfs_client_id_t *nfs4_clientid;
+	struct nfs_client_id_t *nfs4_clientid = NULL;
 
 	lock->lock_sle_type = FSAL_POSIX_LOCK;
 
