909da6b46a6aef0534be3a1c6d4812d121fe02aa
zhitaoli <zhitao.li@iomesh.com>
2025-02-14 16:45:57 +0800

    fix(lock): unlock fsal even though state_hdl.lock_list is empty
    This commit fix the bug in some corner case where we fail to
    unlock from fsal and have removed the record from
    state_hdl.lock_list, retries of unlock will never call FSAL unlock
    again, and no one can acquire the lock anymore.
    
    Change-Id: I1b5cf60cdc91dda6122d9af7c029143ab16b8112
    Signed-off-by: zhitaoli <zhitao.li@iomesh.com>

--- src/SAL/state_lock.c
+++ src/SAL/state_lock.c
@@ -2804,9 +2804,7 @@ state_status_t state_unlock(struct fsal_obj_handle *obj,
 	if (glist_empty(&obj->state_hdl->file.lock_list)) {
 		LogDebug(COMPONENT_STATE,
 			 "Unlock success on file with no locks");
-
-		status = STATE_SUCCESS;
-		goto out_unlock;
+		goto unlock_fsal;
 	}
 
 	LogFullDebug(COMPONENT_STATE,
@@ -2838,6 +2836,7 @@ state_status_t state_unlock(struct fsal_obj_handle *obj,
 		goto out_unlock;
 	}
 
+unlock_fsal:
 	/* Unlocking the entire region will remove any FSAL locks we held,
 	 * whether from fully granted locks, or from blocking locks that were
 	 * in the process of being granted.
