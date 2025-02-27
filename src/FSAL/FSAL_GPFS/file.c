49b350c66373481862770a32776ceadbae8de6f3
Prabhu Murugesan <prabhu.murugesan1@ibm.com>
2025-02-27 00:56:43 -0800

    Fix crash at GPFSFSAL_getattrs()
    Please refer RTC#340891 for more details
    
    Change-Id: I0b869d523863b1af94178cad66c08a1ffbc505e2
    Signed-off-by: Prabhu Murugesan <prabhu.murugesan1@ibm.com>

--- src/FSAL/FSAL_GPFS/file.c
+++ src/FSAL/FSAL_GPFS/file.c
@@ -274,7 +274,7 @@ open_by_handle(struct fsal_obj_handle *obj_hdl, struct state_t *state,
 		goto exit;
 	}
 
-	if (createmode >= FSAL_EXCLUSIVE || (truncated && attrs_out)) {
+	if (attrs_out && (createmode >= FSAL_EXCLUSIVE || truncated)) {
 		/* NOTE: won't come in here when called from gpfs_reopen2...
 		 *       truncated might be set, but attrs_out will be NULL.
 		 */
