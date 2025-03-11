05ab5c4be7e1e96b42722cc357510c99b74023d3
Yogendra Charya Tangalapally <Yogendra.Tangalapally@ibm.com>
2025-03-11 11:47:05 -0700

    Revert "Config: Fix Long Parsing Time for Large Exports"
    This reverts commit addc82c57233a88cdb542d5134e66373fc83a60a.

--- src/config_parsing/analyse.h
+++ src/config_parsing/analyse.h
@@ -7,15 +7,16 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
- *
+ * 
  * ---------------------------------------
  */
 
@@ -63,7 +64,7 @@ struct config_node {
 			char *name;	/* name */
 			struct glist_head sub_nodes;
 		} nterm;
-	} u;
+	}u;
 };
 
 /*
@@ -86,7 +87,6 @@ struct file_list {
 
 struct token_tab {
 	struct token_tab *next;
-	uint64_t token_hashkey;
 	char token[];
 };
 
@@ -105,7 +105,6 @@ struct config_root {
 	char *conf_dir;
 	struct file_list *files;
 	struct token_tab *tokens;
-	struct token_tab *last;
 	uint64_t generation;
 };
 
@@ -138,7 +137,7 @@ void config_error(FILE *fp, const char *filename, int linenum,
 /**
  *  Displays the content of parse tree.
  */
-void print_parse_tree(FILE *output, struct config_root *tree);
+void print_parse_tree(FILE * output, struct config_root *tree);
 
 /**
  * Free resources of parse tree
