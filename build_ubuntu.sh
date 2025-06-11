04cf2ebd8c9199ff47ba275c2080997a2ba060f3
Prabhu Murugesan <prabhu.murugesan1@ibm.com>
2025-06-11 13:26:40 -0700

    IBM-only: Added build scripts for Ubuntu and RHEL
    The src/GANESHA_VERSION file is now used to define
    the Ganesha version, which is then referenced during
    the build process to generate the appropriate RPM
    or Debian package version.
    
    Change-Id: I83799bff3a8dbe4e93160102303e460c41d1e186
    Signed-off-by: Prabhu Murugesan <prabhu.murugesan1@ibm.com>

---
+++ build_ubuntu.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+DIRNAME=$(dirname $0)
+VERFILE=${DIRNAME}/src/GANESHA_VERSION
+
+# Read GANESHA_VERSION file
+. ${VERFILE}
+if (( $? != 0 ))
+then
+    echo "Unable to parse GANESHA_VERSION file. Aborting!!!"
+    exit 1
+fi
+
+# Detect Ubuntu Code name
+if [[ ! -r /etc/os-release ]]
+then
+    echo "Unable to detect build os. Aborting!!!"
+    exit 1
+fi
+. /etc/os-release
+
+case "${UBUNTU_CODENAME}" in
+    focal)
+    OS_RELEASE="~${UBUNTU_CODENAME}"
+    ;;
+
+    jammy)
+    OS_RELEASE="~${UBUNTU_CODENAME}"
+    ;;
+
+    noble)
+    OS_RELEASE="~${UBUNTU_CODENAME}"
+    ;;
+
+    *)
+    echo "Unsupported build os (${UBUNTU_CODENAME}). Aborting!!!"
+    exit 1
+esac
+
+GANESHA_VERSION_STRING="$GANESHA_MAJOR_VERSION.$GANESHA_MINOR_VERSION-$GANESHA_TAG$OS_RELEASE"
+echo "Ganesha packages with version : $GANESHA_VERSION_STRING will be generated."
+
+#Update the changelog file with new Ganesha version and Ubuntu codename
+debchange --newversion $GANESHA_VERSION_STRING --distribution $UBUNTU_CODENAME "NFS Ganesha release $GANESHA_VERSION_STRING for Spectrum Scale" --force-bad-version
+
+# Generate debian/gpfs.python-nfs-ganesha.install based on python version
+if [[ $(python3 -c "import sys; print(sys.version_info >= (3, 12))") == "True" ]]; then
+    # If Python version >= 3.12
+    echo 'usr/local/bin/* /usr/bin/' > debian/gpfs.python-nfs-ganesha.install
+    echo 'usr/local/lib/python*/dist-packages' >> debian/gpfs.python-nfs-ganesha.install
+else
+    # If Python version < 3.12
+    echo 'usr/lib/python*/site-packages' > debian/gpfs.python-nfs-ganesha.install
+fi
+
+rm -r src/debian
+echo "Creating build dependencies ..."
+mk-build-deps --install debian/control
+echo "Building packages ..."
+dpkg-buildpackage -b -us -uc
+
